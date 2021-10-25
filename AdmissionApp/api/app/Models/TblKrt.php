197eda18488a796bfe91956e0c19a6fa30de545e
Muhamad Farkhan <MUHAMAD.FARKHAN@HEXPHARMJAYA.COM>
2021-10-25 15:51:34 +0700

    module new transact - update function

--- AdmissionApp/api/app/Models/TblKrt.php
+++ AdmissionApp/api/app/Models/TblKrt.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TblKrt extends Model
 {
     
-    //public $timestamps = false;
+    public $timestamps = false;
     protected $table = 'tbl_krt';
 
 }
--- AdmissionApp/api/app/Models/Tmpkrt.php
+++ AdmissionApp/api/app/Models/Tmpkrt.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TmpKrt extends Model
 {
    
-    //public $timestamps = false;
+    public $timestamps = false;
     protected $table = 'tmp_krt';
 
 }
